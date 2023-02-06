ad586065aa84b623d955548384186077c58cd276
Kevin Hernandez <kev.hernandezgarza@gmail.com>
2023-02-05 21:27:24 -0600

    initial structure added

---
+++ hernandezGarza.Lab2.cpp
@@ -0,0 +1,32 @@
+//******************************************************************
+// Author: Kevin Hernandez-Garza
+// Assignment Number: Programming Assignment "2"
+// File Name: hernandezGarza.Lab2.cpp
+// Course/Section: COSC 1337 Section -009
+// Due Date: 2/11/23
+// Instructor: Dr.Haytham Mohamed
+//
+// Software Sales: This programs helps calculate the discount, and
+// the total price of the packages depending on how much they purchased.
+//  ******************************************************************
+
+#include <iostream>
+
+main()
+{
+    // double units, total_price, UNIT_PRICE;
+
+    // cout << "How many units are being purchased? "
+    // cin << units << endl;
+    // UNIT_PRICE = 199.00
+
+    // total_price = (units * UNIT_PRICE) * (Discount - 100)
+    // write an if-else statement
+    // if cases
+    // if it's between 10-19 discount is 20%
+    // if it's between 20-49 discount is 30%
+    // if it's between 50-99 discount is 40%
+    // if it's between 100 or more discount is 50%
+    // else if it's between 0-9 discount is 0%
+    return 0;
+}
